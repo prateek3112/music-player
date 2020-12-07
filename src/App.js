d88e4f93a1e9d6ba7d798be7074a68768e00e5a3
prateek3112 <prateekguglani1998@gmail.com>
2020-12-08 00:35:23 +0530

    Dark mode

--- src/App.js
+++ src/App.js
@@ -1,20 +1,38 @@
 import React , {useState} from 'react'
 import Player from "./Components/player";
 import Song from "./Components/song";
+import Button from 'react-bootstrap/Button';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import "../src/styles/App.scss";
 import data from './utils'
 
 function App() {
 
   const [songs,setSongs] = useState(data())
-  const [currentSong,setCurrentsong] = useState(songs[2]);
+  const [currentSong,setCurrentsong] = useState(songs[6]);
   const [isPlaying,setIsPlaying] =useState(false);
+  const [isDark,setIsDark] = useState(false);
+
+const darkModeHandler = ()=>{
+  if(isDark){
+    setIsDark(false)
+  }
+  else{
+    setIsDark(true)
+    
+  }
+}
   return (
-    <div className="App">
+    <div className="App" style={isDark ? {backgroundColor:'Black',color:'#2be09e'}: {}}>
      
 
       <Song currentSong={currentSong}/>
       <Player  currentSong={currentSong} isPlaying={isPlaying} setIsPlaying={setIsPlaying}/>
+      <div className="text-center">
+      <Button onClick={darkModeHandler} className="btn btn-dark d">
+              Dark mode 
+      </Button>
+      </div>
     </div>
   );
 }
--- src/Components/player.jsx
+++ src/Components/player.jsx
@@ -1,4 +1,4 @@
-import React, { useRef } from "react";
+import React, { useRef ,useState } from "react";
 
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
@@ -11,48 +11,59 @@ import {
 const Player = ({ currentSong, isPlaying, setIsPlaying }) => {
   const audioRef = useRef(null);
   const playSongHandler = () => {
-      if(!isPlaying){
-    setIsPlaying(true);
-    audioRef.current.play();
-      }
-      else{
-    
+    if (!isPlaying) {
+      setIsPlaying(true);
+      audioRef.current.play();
+    } else {
       setIsPlaying(false);
       audioRef.current.pause();
     }
   };
+
+  const [songInfo,setSongInfo] = useState({
+    currentTime : null,
+    left : null
+  })
+  const timeUpdateHandler =(e)=>{
+  const  currentTime = e.target.currentTime
+   const  left = e.target.duration - e.target.currentTime
+    setSongInfo(
+      {
+        ...songInfo,currentTime,left
+      }
+     
+    )
+    
+    }
+    const getTime=(time)=>{
+      return (
+        Math.floor(time/60) + ":" + ("0" + Math.floor(time % 60)).slice(-2)
+      )
+    }
   return (
     <div className="player">
       <div className="time-control">
-        <p>Start Time</p>
+  <p>+{getTime(songInfo.currentTime)}</p>
         <input type="range" />
-        <p>end Time</p>
+  <p>-{getTime(songInfo.left)}</p>
       </div>
       <div className="play-control">
         <FontAwesomeIcon className="skip-back" icon={faAngleLeft} size="2x" />
-        
-        {isPlaying ? (
-          <FontAwesomeIcon
-            className="play"
-            onClick={playSongHandler}
-            icon={faPause}
-            size="2x"
-          />
-        ) : (
-          <FontAwesomeIcon
-            className="play"
-            onClick={playSongHandler}
-            icon={faPlay}
-            size="2x"
-          />
-        )}
+
+        <FontAwesomeIcon
+          className="play"
+          onClick={playSongHandler}
+          icon={isPlaying ? faPause : faPlay }
+          size="2x"
+        />
+
         <FontAwesomeIcon
           className="skip-forward"
           icon={faAngleRight}
           size="2x"
         />
       </div>
-      <audio ref={audioRef} src={currentSong.audio}></audio>
+      <audio onLoadedMetadata={timeUpdateHandler} onTimeUpdate={timeUpdateHandler} ref={audioRef} src={currentSong.audio}></audio>
     </div>
   );
 };
